ea8d1e34c4cc2e55f286b4215d4c80a025e9c697
Bruce <recall4056@gmail.com>
2023-10-03 22:15:13 +0800

    feat: init

---
+++ README.md
@@ -0,0 +1,3 @@
+# What to eat?
+
+For people with difficulty making choices, what to eat is a big problem in life!
---
+++ src/App.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+</script>
+
+<main>
+  <div class="relative bg-slate-400 text-lg">What to eat?</div>
+</main>
